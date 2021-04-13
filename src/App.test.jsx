f67a179b2d7a3f0caf8376022abcf5433964883d
tommyrharper <thomasrobertharper@gmail.com>
2021-04-13 04:06:36 +0100

    Add MemoryRouter to App.test.jsx tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import App from './App';
-import { render, screen, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App, { Routes } from './App';
 
 let homeTitleEl;
 let headerEl;
-let footerEl;
 let navEl;
+let navHomeEl;
+let navCatalogEl;
+let navTrackEl;
 
 beforeEach(() => {
-  render(<App />);
+  render(
+    <MemoryRouter initialEntries={['/']} initialIndex={0}>
+      <Routes />
+    </MemoryRouter>,
+  );
   homeTitleEl = screen.getByTestId('home-title');
   headerEl = screen.getByTestId('header');
-  footerEl = screen.getByTestId('footer');
   navEl = screen.getByTestId('nav');
+  navHomeEl = screen.getByTestId('nav-home');
+  navCatalogEl = screen.getByTestId('nav-catalog');
+  navTrackEl = screen.getByTestId('nav-track');
 });
 
 test('Renders without crashing', () => {
@@ -28,6 +37,8 @@ test('Contains the header', () => {
 });
 
 test('Contains the footer', () => {
+  render(<App />);
+  const footerEl = screen.getByTestId('footer');
   expect(footerEl).toBeInTheDocument();
 });
 
@@ -36,9 +47,22 @@ test('Contains the navbar', () => {
 });
 
 test('Stays unchanged if the user clicks home', () => {
-  // Click button
-  fireEvent.click(screen.getByText('Home'));
-
-  const title = screen.getByText(/Items near you:/i);
-  expect(title).toBeInTheDocument();
+  fireEvent.click(navHomeEl);
+  expect(homeTitleEl).toBeInTheDocument();
 });
+
+test('Changes to the Catalogue page if you click on catalog', () => {
+  fireEvent.click(navCatalogEl);
+  const catalogEl = screen.getByTestId('catalog');
+  expect(catalogEl).toBeInTheDocument();
+});
+
+test('Changes to the Order page when you click Track Order', () => {
+  fireEvent.click(navTrackEl);
+  const orderEl = screen.getByTestId('order');
+  expect(orderEl).toBeInTheDocument();
+});
+
+// test('Changes to the Login page when you click the Login button', () => {
+//   fireEvent.CLICK
+// }
