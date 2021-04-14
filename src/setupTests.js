1cbec7f2a974cdec17e097e06e97b5704177cfcc
tommyrharper <thomasrobertharper@gmail.com>
2021-04-14 21:10:08 +0100

    Configure testServer.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,7 @@
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import 'whatwg-fetch'; // May not be necessary
+import './testServer';
 
 configure({ adapter: new Adapter() });
---
+++ src/testServer.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import '@testing-library/jest-dom/extend-expect';
+import 'whatwg-fetch'; // May not be necessary
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import mockProducts from '../config/mockProducts';
+
+const server = setupServer(
+  rest.get('https://api.exchangeratesapi.io/latest', (req, res, ctx) => res(ctx.status(200), ctx.json({ rates: { CAD: 1.42 } }))),
+  rest.get('http://localhost/api/products', (req, res, ctx) => res(ctx.status(200), ctx.json(mockProducts))),
+  rest.get('*', (req, res, ctx) => {
+    console.error(`Please add request handler for ${req.url.toString()}`);
+    return res(
+      ctx.status(500),
+      ctx.json({ error: 'Please add request handler' }),
+    );
+  }),
+);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers);
+
+export { server, rest };
