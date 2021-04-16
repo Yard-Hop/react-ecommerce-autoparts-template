bbdbedfab98eb6446daaded608fc0ceb04d0bf33
tommyrharper <thomasrobertharper@gmail.com>
2021-04-16 03:01:00 +0100

    Add Chart tests

--- src/components/Dashboard/Dashboard.test.jsx
+++ src/components/Dashboard/Dashboard.test.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { fireEvent, render, screen } from '@testing-library/react';
